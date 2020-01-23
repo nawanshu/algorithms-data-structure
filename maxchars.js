305482be8bb54510797bd689196cbe8d5e4e2a46
Nawanshu Choudhary <nawanshu@Nawanshus-MacBook-Air.local>
2020-01-23 12:33:21 +0530

    Maxchar problem

---
+++ maxchars.js
@@ -0,0 +1,21 @@
+function maxChars(str){
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+    for(let char of str) {
+        if(charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+    for(let char in charMap){
+        if(charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
+
+console.log(maxChars("Nawanshu"));
