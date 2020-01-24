91a7d533211a9087012b1fa0a71b533f90a978c0
Nawanshu Choudhary <nawanshu@Nawanshus-MacBook-Air.local>
2020-01-24 15:05:26 +0530

    Steps problem

---
+++ steps.js
@@ -0,0 +1,24 @@
+function steps(n, row=0, stair=''){
+    //Solution 1
+    // for(let row=0;row<n;row++){
+    // let stair = '';
+    //     for(let column=0;column<n;column++){
+    //         if(column<=row) {
+    //             stair += '#';
+    //         } else {
+    //             stair += ' ';
+    //         }
+    //     }
+    //     console.log(stair);
+    // }
+    // if(n === row){
+    //     return;
+    // }
+    // if(n === stair.length){
+    //     console.log(stair);
+    //     return steps(n, row+1);
+    // }
+    // if()
+}
+
+console.log(steps(5))
