ff869bfb78dfae77526f2487b4182e181ce37aaa
Nawanshu Choudhary <nawanshu@Nawanshus-MacBook-Air.local>
2020-01-24 12:51:18 +0530

    chunk problem

---
+++ chunk.js
@@ -0,0 +1,24 @@
+function chunk(array, size){
+    //Solution 1
+    // const chunked = [];
+    // for(let element of array) {
+    //     const last = chunked[chunked.length - 1];
+    //     if(!last || last.length === size) {
+    //         chunked.push([element]);
+    //     } else{
+    //         last.push(element);
+    //     }
+    // }
+    // return chunked;
+
+    //Solution 2
+    const chunked = [];
+    let index = 0;
+    while(index < array.length) {
+        chunked.push(array.splice(index, index+size));
+        index += size;
+    }
+    return chunked;
+}
+
+console.log(chunk([1,2,3], 2));
